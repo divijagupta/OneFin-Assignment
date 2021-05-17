bf611441283c93fc566f707e5c0a3c0aebf2789d
Divija Gupta <divijagupta7@gmail.com>
2021-05-17 12:35:04 +0530

    one model imported and registered in admin.py

--- credyapp/admin.py
+++ credyapp/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from credyapp.models import Movies, Collection
+from credyapp.models import Movies, Collection, RequestCounter
 
 admin.site.register(Movies)
 admin.site.register(Collection)
+admin.site.register(RequestCounter)
